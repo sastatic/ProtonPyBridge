384cd576d465ec2b8c35cc68e459b06f0286046e
Sarwar Ali <sarwaralisiddiquail.com>
2023-12-31 22:40:55 +0530

    update so far but I don't think it's needed

--- cmd/auth.py
+++ cmd/auth.py
@@ -1,1 +1,6 @@
 from common_imports import Session, ProtonError
+
+class ProtonAuth():
+    def __init__(self, username, password):
+        self.UserName = username
+        self.Password = password
